6d756ed9dda10b4795761655b95b61c0b60d7ca2
Luca Crozzoli <crozzoliluca9@gmail.com>
2020-11-16 13:26:32 +0100

    Set the main method for the assignment FizzBuzz

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,19 @@ package dssc.assignment.fizzbuzz;
 
 public class FizzBuzz {
 
-    public static void main(String... args) {
-        
+    public static void main(String[] args) {
+            for (int i = 1; i <= 105; i++) {
+                if (((i % 3) == 0) && ((i % 5) == 0) && ((i % 7) == 0)) { //multiple of 3 5 and 7?
+                    System.out.println("fizzBuzzBang");
+                } else if (((i % 5) == 0) && ((i % 7) == 0)) { // multiple of 5 and 7?
+                    System.out.println("BuzzBang");
+                } else if (((i % 3) == 0) && ((i % 7) == 0)) { //multiple of 3 and 7?
+                    System.out.println("FizzBang");
+                } else if ((i % 7) == 0) {//multiple of 7?
+                    System.out.println("Bang");
+                } else {
+                    System.out.println(i);
+                }
+            }
     }
-
 }
