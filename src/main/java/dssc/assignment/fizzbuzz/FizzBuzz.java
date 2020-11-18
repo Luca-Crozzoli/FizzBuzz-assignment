b6f5c1f69f35dfc289d1b193f81e215aee5b48f6
Luca Crozzoli <crozzoliluca9@gmail.com>
2020-11-18 11:00:54 +0100

    Remove duplication

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -21,14 +21,6 @@ public class FizzBuzz {
     }
 
     public String convert(int number) {
-        if(number ==1){
-          return "1";
-        }
-        else if (number ==2) {
-            return "2";
-        }
-        else{
-            return "41";
-        }
+            return Integer.toString(number);
     }
 }
